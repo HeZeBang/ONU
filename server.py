194d03a8743beb504f9d439388f7e03680e86e94
ZAMBAR <zambar@163.com>
2023-11-19 02:55:16 +0800

    - Modify: markdownlize -> htmlize - Add: Colorful card in msg-box - Add: Special Thanks - Add: Cookies Load/Save

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from ONU_source.template import * # Your ONU File
-__version__ = "v1.0.1"
+__version__ = "v1.0.2-beta"
 
 # ---------------------------- Game Settings From UNO source file ----------------------------------
 
@@ -14,6 +14,23 @@ Available Effect
 '''
 
 game = None
+
+# ------------------------- Default Settings ----------------------------------
+# Those settings will be applied if arguments didn't give
+
+def get_argparser():
+    confparser = argparse.ArgumentParser(description='Initializing ONU game server.')
+    argconf = confparser.add_argument_group("Argument config")
+    argconf.add_argument("-p", "--player-num", type = int, default = 7, help = "Player numbers (if forced started, "
+                         "the remaining slots will be filled with bot)")
+    argconf.add_argument("-hd", "--hand-card-num", type = int, default = 14, help = "Initial hand card numbers")
+    argconf.add_argument("-s", "--special-card-sets", type = int, default = 2, help = "Number of sets of Special Cards")
+    argconf.add_argument("-n", "--numeric-card-sets", type = int, default = 3, help = "Number of sets of Numeric Cards")
+    argconf.add_argument("--port", type = int, default = 8082, help = "Recommend port : 8081 ~ 8088")
+    argconf.add_argument("-r", "--run-on-server", default = False, action="store_true", help = "If this is set True, the server will "
+                                                                                       "not stop running after all players exit.")
+    argconf.add_argument("--password", type = str, default = "123456", help = "Server shutting down password")
+    return confparser
 
 # ---------------------------- Main Program -----------------------------------
 import random
@@ -130,19 +147,19 @@ try:
     from pywebio.session import defer_call, info as session_info, run_async, local, set_env, run_js
     from pywebio.platform import config
     from pywebio.pin import *
+    from pywebio_battery import *
 except:
-    if(input("检测到没有安装依赖 pywebio 输入【y+回车】开始安装") == 'y'):
-        os.system(f"{sys.executable} -m pip install pywebio")
+    if(input("检测到没有安装依赖 pywebio / pywebio_battery 输入 y 并按下【回车 / Enter / Return】开始安装") == 'y'):
+        os.system(f"{sys.executable} -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pywebio pywebio_battery")
         print('安装完成，请重新运行')
     sys.exit() 
+
 MAX_MESSAGES_CNT = 10 ** 4
 
-# async def main():
-    
 online_users = []
 
-def markdownlize(who: str, action: Tuple) -> str:
-    return f"`{who}`  **{action[0].name}**  " + (f"`{repr(action[1])}`" if action[1] is not None else "")
+def htmlize(who: str, action: Tuple) -> str:
+    return f"<code>{who}</code>&emsp;<b>{action[0].name}</b>&emsp;" + (f"{colorful_cards([action[1]])[0]}" if action[1] is not None else "")
 
 async def refresh_msg(my_name):
     """
@@ -152,8 +169,17 @@ async def refresh_msg(my_name):
     last_idx = len(chat_msgs)
     while True:
         await asyncio.sleep(0.5)
+        global cur, game
+
         for m in chat_msgs[last_idx:]:
-            if m[0] != my_name:  # only refresh message that not sent by current user
+            if m[0] == "🎴":
+                put_html('<code>%s</code>: %s' % m, sanitize=True, scope='msg-box')
+            elif m[0] == False:
+                popup('GAME OVER',
+                      [put_markdown(m[1]),
+                      put_table([game.get_scores()],
+                                [_ if _ != "" else "ROBOT" for _ in online_users] + ["ROBOT" for _ in range(max_player_num - len(online_users))])])
+            elif m[0] != my_name:  # only refresh message that not sent by current user
                 put_markdown('`%s`: %s' % m, sanitize=True, scope='msg-box')
         
         # update_status(my_name)
@@ -163,7 +189,6 @@ async def refresh_msg(my_name):
 
         last_idx = len(chat_msgs)
     
-        global cur, game
         notend = True
         cur_id, last_card, is_last_player_drop, plus_two_cnt, hands = game.get_info()
 
@@ -174,14 +199,14 @@ async def refresh_msg(my_name):
                 Player.action = types.MethodType(action_new, Player)
                 local.action = await action_re(hands[cur].get_cards(), last_card, is_last_player_drop)
                 action, info, notend = game.turn()
-                chat_msgs.append(("🎴", markdownlize(f"{my_name}", action)))
+                chat_msgs.append(("🎴", htmlize(f"{my_name}", action)))
                 cur = (info[0] + 1) % max_player_num
                 Player.action = Player.action_old
                 
             elif (len(online_users) > cur and online_users[cur] == "") or (cur >= len(online_users)):
                 # Robot played by Player.action() from your ONU_source
                 action, info, notend = game.turn()
-                chat_msgs.append(("🎴", markdownlize(f"🤖{cur + 1}", action)))
+                chat_msgs.append(("🎴", htmlize(f"🤖{cur + 1}", action)))
                 cur = (info[0] + 1) % max_player_num
                 await asyncio.sleep(0.5)
             
@@ -220,7 +245,7 @@ EFFECTMAPPING = {
     Effect.PLUS_TWO: "➕2️⃣"
 }
 
-def colorful_cards(cards: List[Card]):
+def colorful_cards(cards: List[Card], size: str = "80%"):
     """
     Give the colorfuled card output to a table
     """
@@ -228,11 +253,11 @@ def colorful_cards(cards: List[Card]):
     for card in cards:
         if isinstance(card, NumericCard):
             result.append(
-                (f'<code style="white-space: nowrap; font-size: 120%; background: {COLORMAPPING[card.get_color()][0]}; color: {COLORMAPPING[card.get_color()][1]};">{card.get_color().name} {card.get_number()}</code>'))
+                (f'<code style="white-space: nowrap; font-size: {size}; background: {COLORMAPPING[card.get_color()][0]}; color: {COLORMAPPING[card.get_color()][1]};">{card.get_color().name} {card.get_number()}</code>'))
             
         elif isinstance(card, SpecialCard):
             result.append(
-                (f'<code style="white-space: nowrap; font-size: 120%; background: {COLORMAPPING[card.get_color()][0]}; color: {COLORMAPPING[card.get_color()][1]};">{card.get_color().name} {EFFECTMAPPING[card.get_effect()]}</code>'))
+                (f'<code style="white-space: nowrap; font-size: {size}; background: {COLORMAPPING[card.get_color()][0]}; color: {COLORMAPPING[card.get_color()][1]};">{card.get_color().name} {EFFECTMAPPING[card.get_effect()]}</code>'))
         else:
             pass
 
@@ -266,7 +291,8 @@ def update_status(my_name):
             scope = "score")
     if(local.cards != cardsNew):
         clear("cards")
-        put_html(f'<div style="line-height: 220%">{" ".join(colorful_cards(cardsNew))}</div>', scope = "cards")
+        put_html(f'<div style="line-height: 220%">{" ".join(colorful_cards(cardsNew, "120%"))}</div>', scope = "cards")
+
         
     local.status = statusNew
     local.score = scoreNew
@@ -277,16 +303,19 @@ async def main():
     """
     🃏ONU! Server Edition
     """
-    run_js(f'document.getElementsByClassName("footer")[0].innerHTML="<b>ONU!</b> is powered by PyWebIO"')
+    with redirect_stdout():
+        print("Hello.")
+    
+    run_js(f'document.getElementsByClassName("footer")[0].innerHTML="<b>ONU!</b> is powered by PyWebIO / Special Thanks for <img src="https://contrib.rocks/image?repo=HeZeBang/ONU" style="max-height: 50%;">"')
     
     global chat_msgs, online_users
 
     put_markdown(f"## WELCOME TO 🃏ONU! <sup>{__version__}</sup>")
     put_collapse("README (Click to hide)", put_markdown(
 f"""
-![Latest](https://img.shields.io/github/v/tag/HeZeBang/ONU?label=Latest%20Version) ![Last Update](https://img.shields.io/github/release-date-pre/HeZeBang/ONU?label=Last%20Update)
+[![Static Badge](https://img.shields.io/badge/Github-ONU-black?logo=github&link=https%3A%2F%2Fgithub.com%2FHeZeBang%2FONU)](https://github.com/HeZeBang/ONU) [![Latest](https://img.shields.io/github/v/tag/HeZeBang/ONU?label=Latest%20Version)](https://github.com/HeZeBang/ONU/releases) [![Last Update](https://img.shields.io/github/release-date-pre/HeZeBang/ONU?label=Last%20Update)](https://github.com/HeZeBang/ONU/commits/main)
 
-🎉 Welcome to `ONU!` <sup>Server Edition</sup>, a game designed for everyone in the **SI100B** course.
+🎉 Welcome to `ONU!`, a game designed for everyone in the **SI100B** course.
 
 **The core logic of this game stems from your very own `ONU` class!** 
 We've taken the initiative to adapt certain modules to ensure seamless gameplay on your web browser. 
@@ -300,8 +329,6 @@ Simply have them connect to the **WIFI:** 🌐 `ShanghaiTech` and open the follo
 
 ***References:***
 [Chat_Room - PyWebIO Demo](https://github.com/wang0618/PyWebIO/blob/dev/demos/chat_room.py)
-
-***ZAMBAR***
 """), open = True)
 
     put_row([
@@ -315,9 +342,14 @@ Simply have them connect to the **WIFI:** 🌐 `ShanghaiTech` and open the follo
             ],
             size='72% 5px 28%')
     try:
-        nickname = await input("What's your name...", required=True,
-            validate=lambda n: 'Nickname already exists!' 
-                if n in online_users or n == '📢' else None)
+        nickname = await get_cookie('name')
+        if(nickname is None or nickname in online_users):
+            nickname = await input("What's your name...", required=True,
+                validate=lambda n: 'Nickname already exists!' 
+                    if n in online_users or n == '📢' else None)
+        else:
+            toast(f"🎉Welcome back, {nickname}")
+        set_cookie("name", nickname, 7)
     except:
         return
     
@@ -344,12 +376,12 @@ Simply have them connect to the **WIFI:** 🌐 `ShanghaiTech` and open the follo
             else :
                 sys.exit()
 
-    put_markdown("## Chat with friends")
+    put_markdown("## 💬 Chat with friends")
     put_input('chatbox')
-    put_actions(name = "actbar", label = "", buttons = ['Send', 'Exit Game',{'label': 'Shutdown Server','type' : 'submit',
+    put_actions(name = "actbar", label = "", buttons = ['Send', 'Exit Game',{'label': '🛑Shutdown Server','type' : 'submit',
                                                                              'value': 'Shutdown Server', 'color': 'danger'}])
     update_status(nickname)
-    put_markdown("## Your Cards")
+    put_markdown("## 🎴 Your Cards")
     put_scope("cards")
     local.cards=[]
     update_status(nickname)
@@ -380,22 +412,6 @@ def reset_game() :
     global cur, game
     game = Game(allKindofCards(), max_player_num, hand_card_num, 0)
     cur = 0
-
-# ---------------------------- Settings From Parse ----------------------------------
-
-def get_argparser():
-    confparser = argparse.ArgumentParser(description='Initializing ONU game server.')
-    argconf = confparser.add_argument_group("Argument config")
-    argconf.add_argument("-p", "--player-num", type = int, default = 7, help = "Player numbers (if forced started, "
-                         "the remaining slots will be filled with bot)")
-    argconf.add_argument("-hd", "--hand-card-num", type = int, default = 14, help = "Initial hand card numbers")
-    argconf.add_argument("-s", "--special-card-sets", type = int, default = 2, help = "Number of sets of Special Cards")
-    argconf.add_argument("-n", "--numeric-card-sets", type = int, default = 3, help = "Number of sets of Numeric Cards")
-    argconf.add_argument("--port", type = int, default = 8082, help = "Recommend port : 8081 ~ 8088")
-    argconf.add_argument("-r", "--run-on-server", default = False, action="store_true", help = "If this is set True, the server will "
-                                                                                       "not stop running after all players exit.")
-    argconf.add_argument("--password", type = str, default = "123456", help = "Server shutting down password")
-    return confparser
     
 if __name__ == '__main__':
     args = get_argparser().parse_args()
